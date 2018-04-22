fa08c44b0319a30b18bf05cae98daf76d03122f7
Boone Wang <boone.wang@outlook.com>
2018-04-22 21:46:16 +0800

    Initial commit
    Signed-off-by: Boone Wang <boone.wang@outlook.com>

--- CutWord.py
+++ CutWord.py
@@ -1,3 +1,5 @@
+# Updated on Sep 04, 2018
+
 import jieba
 import thulac
 
@@ -5,13 +7,18 @@ import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("558517626411.csv", index_col=0)
+# Read comments from file
+df = pd.read_csv("568230268941.csv", index_col=0, encoding="gbk")
 str = ""
 
+# Remove blank comment
 comments = df[df["rateContent"] != "此用户没有填写评论!"]["rateContent"]
 
+# Connect all comment in one string
 for i in comments:
     str += i + ","
+
+# Split Wording
 
 # Jieba Engine
 # word_list_jieba = jieba.cut(str, cut_all=False)
@@ -22,10 +29,8 @@ for i in comments:
 thu1 = thulac.thulac(seg_only=True, filt=True)
 word_list = thu1.cut(str, text=True)
 
-my_wc = WordCloud(font_path="STHeiti Medium.ttc", width=600, height=600, background_color="white").generate(word_list)
+# Print Word Cloud
+my_wc = WordCloud(font_path="STXIHEI.TTF", width=600, height=600, background_color="white").generate(word_list)
 plt.imshow(my_wc)
 plt.axis("off")
 plt.show()
-
-# with open("temp.csv", mode="w", encoding="utf16") as file:
-#     file.write(word_list)
--- GetComment.py
+++ GetComment.py
@@ -1,3 +1,5 @@
+# Updated on Sep 04, 2018
+
 import urllib.request as urlrequest
 import json
 import re
@@ -9,6 +11,7 @@ from pandas.io.json import json_normalize
 # ITEM_ID = "546408734932"
 # ITEM_ID = "558185566672"
 ITEM_ID = "558517626411"  # Vapromax
+ITEM_ID = "568230268941"  # Pegasus 33
 
 
 # ITEM_ID = "39356129215" #关门神器
@@ -29,14 +32,16 @@ def read_url(item_id, page, try_times=2):
     url_template = "https://rate.tmall.com/list_detail_rate.htm?itemId={0}&spuId=520897750&sellerId=890482188&order=3&currentPage={1}"
     url = url_template.format(item_id, page)
     content = urlrequest.urlopen(url).read()
-    content = content.decode("gbk")
+    content = content.decode("utf8")
+    content = content[13:]
+    content = content[:-1]
 
     # Tmall is in json format with additional disturbing information
     # using regular to remove the additional info
-    cleaned_content = re.findall("\\{.*?\\}$", content)
+    # cleaned_content = re.findall("\\{.*?\\}$", content)
 
-    if cleaned_content.__len__() > 0:
-        return json.loads(cleaned_content[0])
+    if content.__len__() > 0:
+        return json.loads(content)
     else:
         # In case failed by Taobao Security Matrix, then try within try times limitation
         time.sleep(random.randint(15, 25) / 10)
@@ -45,7 +50,7 @@ def read_url(item_id, page, try_times=2):
 
 # First Read
 json_content = read_url(ITEM_ID, 1)
-pages = int(json_content["paginator"]["lastPage"])
+pages = int(json_content["rateDetail"]["paginator"]["lastPage"])
 df_rate = pd.DataFrame()
 print("*** Total {} Pages ***".format(pages))
 
@@ -53,7 +58,7 @@ print("*** Total {} Pages ***".format(pages))
 for i in range(1, pages + 1):
     json_content = read_url(ITEM_ID, i)
     if json_content != "":
-        df = json_normalize(json_content["rateList"])
+        df = json_normalize(json_content["rateDetail"]["rateList"])
         df_rate = df_rate.append(df, True)
 
         print('---Page {:0>2} read---'.format(i))
